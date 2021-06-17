16a7334d01619d26c4c7c8d720ee41daf7256bd0
Lena Hou <lenahou2013@gmail.com>
2021-06-16 21:19:34 -0400

    commit tree 3b3f4a139a9564966dbabff5251bd9ee864e9425

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,45 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.create(params)
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by(:id => params[:id])
+  
+    erb :show
+  end
+
+ # load the edit form by matching the recipe id
+ get "/recipes/:id/edit" do
+  @recipe = Recipe.find_by(:id => params[:id])
+  erb :edit
+end
+
+patch "/recipes/:id" do
+  @recipe = Recipe.find(params[:id])
+  if @recipe.update(params)
+    redirect to "/recipes/#{@recipe.id}"
+  end
+end
+
+
+ delete '/recipes/:id' do
+  @recipe = Recipe.find_by_id(params[:id])
+  @recipe.delete
+  
+  redirect to '/recipes'
+ end
 
 end
